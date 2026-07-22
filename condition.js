64042122b2bbbec649084213660627d52944287f
abdulahadwork009-stack <abdulahad.work009@gmail.com>
2026-07-23 00:14:34 +0500

    Add even-odd and largest-of-three logic

---
+++ condition.js
@@ -0,0 +1,22 @@
+// Number is Even or Odd 
+
+let n = 40;
+if(n%2==0){
+    console.log("It is even number",n);
+}else{
+    console.log("It is odd number", n);
+}
+
+
+// Find the largest Number
+
+let x=10;
+let y=20;
+let z=30;
+if(x >= y && x >= z){
+    console.log("x is largest number", x);
+}else if(y >= x && y>=z){
+    console.log("y is the largest number",y);
+}else{
+    console.log("z  is the largest number =",z);
+}
