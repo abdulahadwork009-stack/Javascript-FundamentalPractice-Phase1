02d83d76fb85b4a00149cde5f9f41b43aea60db7
abdulahadwork009-stack <abdulahad.work009@gmail.com>
2026-07-23 01:59:26 +0500

    Add loop problems: print 1-100, even numbers, multiplication table

---
+++ problems.js
@@ -0,0 +1,36 @@
+//Print Hello
+console.log("Hello,World!")
+
+//Addition
+let a=5;
+let b=14;
+let add=a+b;
+console.log(add);
+
+//Subtraction
+let x=7;
+let y=4;
+let sub=x-y;
+console.log(sub);
+
+//Multiplication
+let c=9;
+let d=6;
+let mul=c*d;
+console.log(mul);
+
+//Diviosn
+let z=9;
+let k=4;
+let div=z/k;
+console.log(div);
+
+//Swaping 
+let u = 20;
+let v = 5;
+console.log("before swap:", u, v);
+
+u = u + v;
+v = u - v;
+u = u - v;
+console.log("after swap:", u, v);
